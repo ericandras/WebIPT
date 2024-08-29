7a370005c1e25358b74804fc64d5bd60da599925
Eric Andrade de Jesus <eric.jesus2023@gmail.com>
2024-08-28 21:48:07 -0300

    feat: listening port 3000

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 import express from "express";
+import ht from "http"
+import path from "path";
+import { fileURLToPath } from 'url';
 
 const app = express()
+const http = ht.createServer(app);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/src/index.html');
+})
+
+app.use(express.static('public'))
+
+http.listen(process.env.PORT || 3000, () => {
+    console.log('listening on port: 3000');
+    console.log(" "+ new Date);
+})
