52b5da0dd0a0e73f67400419d47a0f68eccf94fb
Lucas Caracoci <lucascaracoci2@gmail.com>
2024-09-14 09:20:03 -0300

    cores

--- Interface/src/components/main.jsx
+++ Interface/src/components/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './main.css'
 function Main() {
+  
   return (
     <div className='teste'>
      <div className='container'><h2>testeee</h2><div className='bola'></div></div>
