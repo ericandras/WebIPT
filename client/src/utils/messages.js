8a857b015e96d5f0267ecd44f12a71b51a92a40c
Lucas Caracoci <lucascaracoci2@gmail.com>
2024-11-16 12:16:09 -0300

    teste

--- client/src/utils/messages.js
+++ client/src/utils/messages.js
@@ -1,7 +1,4 @@
 
-import io from "socket.io-client";
-console.log(`http://${import.meta.env.VITE_IP_SERVER}:4000`)
-const socket = io.connect(`http://${import.meta.env.VITE_IP_SERVER}:4000`); 
 
 function sendMessage(message) {
     socket.emit("input_command", { command: message });
