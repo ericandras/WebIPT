3000af5e82b98436a8197dec505a3b404a2d9b53
Manoel Rocha <guevurds@gmail.com>
2025-01-30 20:31:06 -0300

    fix: remove sudo command

--- instalador.sh
+++ instalador.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 # Atualiza os pacotes do sistema
-sudo apt-get update -y && sudo apt-get upgrade -y
+apt-get update -y && apt-get upgrade -y
 
 # Instala as dependências necessárias
-sudo apt-get install -y curl gnupg
+apt-get install -y curl gnupg
 
 # Adiciona o repositório do Node.js (LTS recomendado para estabilidade)
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_lts.x | -E bash -
 
 # Instala o Node.js e o gerenciador de pacotes npm
-sudo apt-get install -y nodejs
+apt-get install -y nodejs
 
 # Verifica a instalação do Node.js e npm
 node -v
 npm -v
 
 # Adiciona a chave GPG e o repositório do Yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 # Atualiza os repositórios e instala o Yarn
-sudo apt-get update -y
-sudo apt-get install -y yarn
+apt-get update -y
+apt-get install -y yarn
 
 # Verifica a instalação do Yarn
 yarn -v
