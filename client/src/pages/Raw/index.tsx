bdc2763cb2a633e8f7ab24916cc82d6ee39bfb2c
Manoel Rocha <guevurds@gmail.com>
2025-02-18 04:39:45 -0300

    table raw

--- client/src/pages/Raw/index.tsx
+++ client/src/pages/Raw/index.tsx
@@ -7,10 +7,46 @@ function Raw() {
     prerouting: {
       NOTRACK: {
         required: getOptionsChain([]),
+        optional: getOptionsChain(['-p', '-s', '-d', '-i']),
+        info: 'Desativa o rastreamento de conexão (conntrack) para pacotes correspondentes.',
+      },
+      ACCEPT: {
+        required: getOptionsChain([]),
+        optional: getOptionsChain(['-p', '-s', '-d', '-i']),
+        info: 'Permite o pacote sem realizar modificações.',
+      },
+      DROP: {
+        required: getOptionsChain([]),
+        optional: getOptionsChain(['-p', '-s', '-d', '-i']),
+        info: 'Descarta o pacote.',
+      },
+      RETURN: {
+        required: getOptionsChain([]),
+        optional: getOptionsChain([]),
+        info: 'Retorna o controle para a chain anterior, sem modificar o pacote'
+      }
+    },
+    output: {
+      NOTRACK: {
+        required: getOptionsChain([]),
         optional: getOptionsChain(['-p', '-s', '-d', '-o']),
         info: 'Desativa o rastreamento de conexão (conntrack) para pacotes correspondentes.',
       },
-
+      ACCEPT: {
+        required: getOptionsChain([]),
+        optional: getOptionsChain(['-p', '-s', '-d', '-o']),
+        info: 'Permite o pacote sem realizar modificações.',
+      },
+      DROP: {
+        required: getOptionsChain([]),
+        optional: getOptionsChain(['-p', '-s', '-d', '-o']),
+        info: 'Descarta o pacote.',
+      },
+      RETURN: {
+        required: getOptionsChain([]),
+        optional: getOptionsChain([]),
+        info: 'Retorna o controle para a chain anterior, sem modificar o pacote'
+      }
     }
   }
   return <SkeletonTable title="FILTER" chainOptions={chainOptions}/>
